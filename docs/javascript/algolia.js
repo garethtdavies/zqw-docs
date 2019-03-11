16bc22411981493ceef7f8cfc898a2cdedba9a95
Gareth Davies <hello@garethtdavies.com>
2019-03-11 12:53:58 -0700

    Algolia bootstrap

---
+++ docs/javascript/algolia.js
@@ -0,0 +1,6 @@
+docsearch({
+    apiKey: 'dcce540082948eb15b7074f3ecd465d5',
+    indexName: 'zecqtwallet',
+    inputSelector: '.md-search__input',
+    debug: false // Set debug to true if you want to inspect the dropdown
+});
